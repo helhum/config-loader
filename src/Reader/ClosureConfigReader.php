776188b369ab2bf8ae52c90d2004b3eea3eb4996
Helmut Hummel <info@helhum.io>
2018-01-26 23:06:18 +0100

    [FEATURE] Introduce ClosureConfigReader
    With this reader one can provide closures for hasConfig and readConfig
    to achieve maximum flexibility

---
+++ src/Reader/ClosureConfigReader.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+namespace Helhum\ConfigLoader\Reader;
+
+/*
+ * This file is part of the helhum configuration loader package.
+ *
+ * (c) Helmut Hummel <info@helhum.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * I can do anything reader.
+ * Extremely useful in tests, but also when interacting with arbitrary frameworks.
+ */
+class ClosureConfigReader implements ConfigReaderInterface
+{
+    /**
+     * @var \Closure
+     */
+    private $readConfigClosure;
+
+    /**
+     * @var \Closure
+     */
+    private $hasConfigClosure;
+
+    public function __construct(\Closure $readConfigClosure, \Closure $hasConfigClosure = null)
+    {
+        $this->readConfigClosure = $readConfigClosure;
+        $this->hasConfigClosure = $hasConfigClosure;
+    }
+
+    public function hasConfig(): bool
+    {
+        if ($this->hasConfigClosure) {
+            return ($this->hasConfigClosure)();
+        }
+        return count($this->readConfig()) > 0;
+    }
+
+    public function readConfig(): array
+    {
+        return ($this->readConfigClosure)();
+    }
+}
